498bccb0856e5c6ffaa2f00f6c40af3906e79d15
david <davmwa90@gmail.com>
2023-02-04 10:29:58 +0300

    updated components

--- src/components /Contact.js
+++ src/components /Contact.js
@@ -7,7 +7,6 @@ function Contact() {
         <p>We are located @ Naivasha Road, off Ngong Road Nairobi</p><br/>
         <p>Cell: 254113312554</p><br/>
         <p>Email: dfurniture@furniture.com</p>
-
       </section>
     </div>
   )
